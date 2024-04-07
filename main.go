17af6791f9051447b59f10838a1b8ed59f1a5088
doston <dostonshernazarov2001@gmail.com>
2024-04-07 11:19:00 +0500

    web chat websocket

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var clients []websocket.Conn
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) 
+
+		clients = append(clients, *conn)
+
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			for _, client := range clients {
+				if err = client.WriteMessage(msgType, msg); err != nil {
+					return
+				}
+			}
+
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
